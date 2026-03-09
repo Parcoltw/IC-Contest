cc6dd85c1878e062986bf6f18e6cf838e2f192de
Parcol <134140133+Parcoltw@users.noreply.github.com>
2026-03-09 18:33:33 +0800

    Update dc_syn.tcl

--- 2018_HC/dc_syn.tcl
+++ 2018_HC/dc_syn.tcl
@@ -1,19 +1,24 @@
-# Read all Files
-read_verilog huffman.v
-current_design huffman
+#Read All Files
+read_file -format verilog  huffman.v
+#read_file -format sverilog  huffman.v
+current_design CONVEX
 link
 
-# Setting Clock Constraits
+#Setting Clock Constraints
 source -echo -verbose huffman.sdc
+check_design
+set high_fanout_net_threshold 0
+uniquify
+set_fix_multiple_port_nets -all -buffer_constants [get_designs *]
 
-# Synthesis all design
-compile -map_effort high -area_effort high
-compile -map_effort high -area_effort high -inc
+#Synthesis all design
+#compile -map_effort high -area_effort high
+#compile -map_effort high -area_effort high -inc
+compile_ultra
 
-write -format ddc  -hierarchy -output "huffman_syn.ddc"
-write_sdf huffman_syn.sdf
-write_file -format verilog -hierarchy -output huffman_syn.v
+write -format ddc     -hierarchy -output "huffman_syn.ddc"
+write_sdf -version 1.0  huffman_syn.sdf
+write -format verilog -hierarchy -output huffman_syn.v
 report_area > area.log
 report_timing > timing.log
-report_qor > huffman_syn.qor
-
+report_qor   >  huffman_syn.qor
