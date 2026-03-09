100bc99d611915da376619f5d0695657cabff220
Parcol <134140133+Parcoltw@users.noreply.github.com>
2026-03-09 18:40:08 +0800

    Update dc_syn.tcl

--- 2016_CLE/dc_syn.tcl
+++ 2016_CLE/dc_syn.tcl
@@ -1,21 +1,24 @@
 #Read All Files
-read_verilog ./CLE.v
+read_file -format verilog  CLE.v
+#read_file -format sverilog  CLE.v
 current_design CLE
 link
 
 #Setting Clock Constraints
-source -echo -verbose CLE_DC.sdc
+source -echo -verbose CLE.sdc
+check_design
+set high_fanout_net_threshold 0
+uniquify
+set_fix_multiple_port_nets -all -buffer_constants [get_designs *]
 
 #Synthesis all design
-compile -map_effort high -area_effort high
-compile -map_effort high -area_effort high -inc
-
-
+#compile -map_effort high -area_effort high
+#compile -map_effort high -area_effort high -inc
+compile_ultra
 
 write -format ddc     -hierarchy -output "CLE_syn.ddc"
-write_sdf CLE_syn.sdf
-write_file -format verilog -hierarchy -output CLE_syn.v
+write_sdf -version 1.0  CLE_syn.sdf
+write -format verilog -hierarchy -output CLE_syn.v
 report_area > area.log
 report_timing > timing.log
 report_qor   >  CLE_syn.qor
-
