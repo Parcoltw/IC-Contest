f352aa403edf29887e1b72cbb92a2a1153d81933
Parcol <134140133+Parcoltw@users.noreply.github.com>
2026-03-10 09:12:26 +0800

    Update dc_syn.tcl

--- 2024_BICUBIC/dc_syn.tcl
+++ 2024_BICUBIC/dc_syn.tcl
@@ -11,7 +11,7 @@ check_design
 set high_fanout_net_threshold 0
 uniquify
 set_fix_multiple_port_nets -all -buffer_constants [get_designs *]
-#set_max_area 0
+
 #Synthesis all design
 #compile -map_effort high -area_effort high
 #compile -map_effort high -area_effort high -inc
